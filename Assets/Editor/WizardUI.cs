0a8becc16fe7b120aae4826c5f40e1e77b109aeb
Chema Damak <chema.damak@unity3D.com>
2020-02-20 13:53:18 +0100

    Creation of Wizard UI

---
+++ Assets/Editor/WizardUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+using UnityEditor.UIElements; 
+using UnityEngine.UIElements; 
+
+public class WizardUI : EditorWindow
+{
+  
+    [MenuItem("WizardUI/Open _%#T")]
+    public static void ShowWindow()
+    {
+        // Opens the window, otherwise focuses it if it’s already open.
+        var window = GetWindow<WizardUI>();
+
+        // Adds a title to the window.
+        window.titleContent = new GUIContent("WizardUI");
+
+        // Sets a minimum size to the window.
+        window.minSize = new Vector2(250, 50);
+    }
+    
+
+}
